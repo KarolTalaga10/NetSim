e8a26ca42dad6725eb43a0cd501ad6f4ce5e534a
Mateusz <inpg.bahyrycz@gmail.com>
2019-12-17 22:09:51 +0100

    all tests implemented

---
+++ NetSim/test/package_test.cpp
@@ -0,0 +1,48 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "package.hpp"
+
+/********************************
+ *                              *
+ * DZIALA TYLKO POJEDYNCZO !!!  *
+ *                              *
+ ********************************/
+
+TEST(PackageInitTest, firstPackageID) {
+    Package pack1;
+
+    EXPECT_EQ(1, pack1.get_id());
+}
+
+TEST(PackageAssignTest, secondPackageID) {
+    Package pack1;
+    Package pack2;
+
+    EXPECT_EQ(2, pack2.get_id());
+}
+
+
+TEST(PackageCopyTest, changePackageID) {
+    Package pack1;
+    Package pack2(static_cast<Package&&>(pack1));
+
+    EXPECT_EQ(1, pack2.get_id());
+}
+
+TEST(PackageOperatorTest, changingPackageID) {
+    Package pack1;
+    Package pack2;
+
+    pack2.operator=(static_cast<Package&&>(pack1));
+
+    EXPECT_EQ(1, pack2.get_id());
+}
+
+TEST(PackageidTest, changePackageID){
+    Package pack1;
+    Package pack2;
+
+    EXPECT_TRUE(pack1.get_id() != pack2.get_id());
+}
+
