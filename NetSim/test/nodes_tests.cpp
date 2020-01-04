0c4ca416bcb04106dfd0ae936a70d32ffd3a3162
Mateusz <inpg.bahyrycz@gmail.com>
2020-01-04 09:26:00 +0100

    test example added

--- NetSim/test/nodes_tests.cpp
+++ NetSim/test/nodes_tests.cpp
@@ -2,6 +2,38 @@
 #include "gmock/gmock.h"
 
 #include "nodes.hpp"
+/*
+---------------- TEST EXAMPLE ----------------
+2 Workers: w1(FIFO), w2(LIFO)
+2 Ramps: r1, r2
+2 Storehouses: s1(FIFO), s2(LIFO)
+
+TEST(FunctionalityName, TestingCase) {
+    PackageQueue q1(PackageQueueType::FIFO);
+	PackageQueue q2(PackageQueueType::LIFO);
+	PackageQueue q3(PackageQueueType::FIFO);
+	PackageQueue q4(PackageQueueType::LIFO);
+    std::unique_ptr<IPackageQueue> ptr1 = std::make_unique<PackageQueue>(q1);
+    std::unique_ptr<IPackageQueue> ptr2 = std::make_unique<PackageQueue>(q2);
+    std::unique_ptr<IPackageStockpile> ptr3 = std::make_unique<PackageQueue>(q3);
+    std::unique_ptr<IPackageStockpile> ptr4 = std::make_unique<PackageQueue>(q4);
+	Storehouse s1(1, std::move(ptr3));
+	Storehouse s2(2, std::move(ptr4));
+	Ramp r1(1, 1);
+	Ramp r2(2, 2);
+	Worker w1(1, 1, std::move(ptr1));
+	Worker w2(2, 2, std::move(ptr2));
+    //-----------------------------------------------
+    // make your own preferences
+    //e.x. r1.mReceiverPreferences.add_receiver(&w1);
+    //-----------------------------------------------
+    // test logic
+
+    EXPECT_EQ(WhatWeExpect, FunctionCall);
+}
+ */
+
+
 
 TEST(StorehouseTest, CorrectReceivingTest) {
     PackageQueue q(PackageQueueType::FIFO);
